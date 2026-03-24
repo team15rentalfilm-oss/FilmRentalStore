0d4a4a38a66a6ca03e32f06d7d2953ba7995993a
UTSAV SARDA <utsavsarda10@gmail.com>
2026-03-24 14:39:27 +0530

    Store Entity

--- src/main/java/com/iem/FilmRentalStore/entity/Store.java
+++ src/main/java/com/iem/FilmRentalStore/entity/Store.java
@@ -1,5 +1,38 @@
 package com.iem.FilmRentalStore.entity;
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
 
+@Entity
+@Table(name = "store")
 public class Store {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "store_id", columnDefinition = "TINYINT UNSIGNED")
+    private Integer storeId;
+
+    @Column(name = "manager_staff_id", nullable = false, columnDefinition = "TINYINT UNSIGNED")
+    private Integer managerStaffId;
+
+    @Column(name = "address_id", nullable = false, columnDefinition = "SMALLINT UNSIGNED")
+    private Integer addressId;
+
+    @Column(name = "last_update", insertable = false, updatable = false)
+    private LocalDateTime lastUpdate;
+
+    // Default Constructor
+    public Store() {}
+
+    // Getters and Setters
+    public Integer getStoreId() { return storeId; }
+    public void setStoreId(Integer storeId) { this.storeId = storeId; }
+
+    public Integer getManagerStaffId() { return managerStaffId; }
+    public void setManagerStaffId(Integer managerStaffId) { this.managerStaffId = managerStaffId; }
+
+    public Integer getAddressId() { return addressId; }
+    public void setAddressId(Integer addressId) { this.addressId = addressId; }
+
+    public LocalDateTime getLastUpdate() { return lastUpdate; }
+    public void setLastUpdate(LocalDateTime lastUpdate) { this.lastUpdate = lastUpdate; }
 }
