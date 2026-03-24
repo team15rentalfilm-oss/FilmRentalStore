f5ad85360d755c060a89c60214054126b894429e
Mafuj-dev <gazimafuj95@gmail.com>
2026-03-24 10:01:34 +0530

    Initial commit

---
+++ src/main/java/com/iem/FilmRentalStore/FilmRentalStoreApplication.java
@@ -0,0 +1,15 @@
+package com.iem.FilmRentalStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import lombok.Getter;
+
+@SpringBootApplication
+public class FilmRentalStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmRentalStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iem/FilmRentalStore/FilmRentalStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iem.FilmRentalStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmRentalStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
