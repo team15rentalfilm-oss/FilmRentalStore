28ec6371390a0c2d2cd9ef4946aeaed349c518c4
syash689 <121748627+syash689@users.noreply.github.com>
2026-03-24 13:25:52 +0530

    first

--- src/main/java/com/iem/FilmRentalStore/entity/Customer.java
+++ src/main/java/com/iem/FilmRentalStore/entity/Customer.java
@@ -1,13 +1,10 @@
 package com.iem.FilmRentalStore.entity;
 
 import jakarta.persistence.*;
-import lombok.Data;
-
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "customer")
-@Data
 public class Customer {
 
     @Id
@@ -51,5 +48,77 @@ public class Customer {
     @PreUpdate
     public void preUpdate() {
         lastUpdate = LocalDateTime.now();
+    }
+
+    public Short getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Short customerId) {
+        this.customerId = customerId;
+    }
+
+    public Byte getStoreId() {
+        return storeId;
+    }
+
+    public void setStoreId(Byte storeId) {
+        this.storeId = storeId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Short getAddressId() {
+        return addressId;
+    }
+
+    public void setAddressId(Short addressId) {
+        this.addressId = addressId;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    public LocalDateTime getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(LocalDateTime createDate) {
+        this.createDate = createDate;
+    }
+
+    public LocalDateTime getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public void setLastUpdate(LocalDateTime lastUpdate) {
+        this.lastUpdate = lastUpdate;
     }
 }
