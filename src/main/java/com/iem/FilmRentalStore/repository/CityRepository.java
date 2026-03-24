f7f7d24e6abbc632df3c80148ecbc15228b5fa56
Mafuj-dev <gazimafuj95@gmail.com>
2026-03-24 12:31:26 +0530

    controller fix

--- src/main/java/com/iem/FilmRentalStore/repository/CityRepository.java
+++ src/main/java/com/iem/FilmRentalStore/repository/CityRepository.java
@@ -1,5 +1,11 @@
 package com.iem.FilmRentalStore.repository;
 
-public class CityRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.iem.FilmRentalStore.entity.City;
+
+@Repository
+public interface CityRepository extends JpaRepository<City, Short> {
 
 }
