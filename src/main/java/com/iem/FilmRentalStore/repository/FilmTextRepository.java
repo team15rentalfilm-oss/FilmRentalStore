9bdd740a7cf422eef00827e4d441f204cc4c80b6
syash689 <121748627+syash689@users.noreply.github.com>
2026-03-24 23:22:33 +0530

    Filmtext and Inventory

--- src/main/java/com/iem/FilmRentalStore/repository/FilmTextRepository.java
+++ src/main/java/com/iem/FilmRentalStore/repository/FilmTextRepository.java
@@ -1,5 +1,10 @@
 package com.iem.FilmRentalStore.repository;
 
-public class FilmTextRepository {
+import com.iem.FilmRentalStore.entity.FilmText;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
 
+@Repository
+public interface FilmTextRepository extends JpaRepository<FilmText, Integer>, JpaSpecificationExecutor<FilmText> {
 }
--- src/main/java/com/iem/FilmRentalStore/repository/InventoryRepository.java
+++ src/main/java/com/iem/FilmRentalStore/repository/InventoryRepository.java
@@ -1,5 +1,10 @@
 package com.iem.FilmRentalStore.repository;
 
-public class InventoryRepository {
+import com.iem.FilmRentalStore.entity.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
 
+@Repository
+public interface InventoryRepository extends JpaRepository<Inventory, Integer>, JpaSpecificationExecutor<Inventory> {
 }
