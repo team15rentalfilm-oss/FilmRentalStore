2903ed84c0ba5bff5315834469eba98346d489f6
UTSAV SARDA <utsavsarda10@gmail.com>
2026-03-26 09:42:14 +0530

    FilmActorController updated

--- src/main/java/com/iem/FilmRentalStore/controller/FilmActorController.java
+++ src/main/java/com/iem/FilmRentalStore/controller/FilmActorController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 @RestController
-@RequestMapping({"/api/film-actor", "/film-actor"})
+@RequestMapping("/api/film-actor")
 public class FilmActorController {
 
     private final FilmActorService service;
